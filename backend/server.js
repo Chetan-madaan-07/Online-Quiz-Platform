35a5ba73eb8bb9c843ded7d82f7bd2ff9eb76396
chetan <chetanmadaan903@gmail.com>
2025-12-31 20:11:47 +0530

    little work on backend
    start of database

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// DB connect
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Quiz Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
